9910c3f5346f24ec7be436516c01524fbc944b2d
Isa <isabel.louzada07@gmail.com>
2024-12-16 18:32:33 -0300

    Adicionar aos dados mais livros

--- dados.js
+++ dados.js
@@ -10,40 +10,48 @@ let dados = [
     título: 'Pessoas Normais',
     descricao: '"Pessoas Normais" é um romance de Sally Rooney que acompanha a intensa e complexa relação entre Marianne e Connell, dois jovens irlandeses que, apesar de se conhecerem desde a adolescência, lutam para construir uma conexão duradoura em meio às pressões sociais e às suas próprias inseguranças. A história explora temas como classe social, identidade e o primeiro amor de forma profunda e realista.',
     link: 'https://www.amazon.com.br/Pessoas-normais-Sally-Rooney/dp/8535932569',
-    },
+  },
 
-    {
+  {
         título: 'Uma questão de química',
         descricao: 'Elizabeth Zott é uma renomada química que enfrenta a mentalidade da época e, no que parece um simples programa de culinária, desperta o pensamento crítico das donas de casa. Uma história emocionante sobre amor, perda e a busca por um novo começo.',
         link: 'https://www.amazon.com.br/Uma-quest%C3%A3o-qu%C3%ADmica-Bonnie-Garmus/dp/6555652888'
-    },
+  },
     
-    {
+  {
             título: 'A Marca de Atena',
             descricao: 'Uma aventura épica ambientada na Grécia Antiga, repleta de mistérios e ação.',
             link: 'https://www.amazon.com.br/Marca-Atena-Rick-Riordan/dp/8580573106' // Substitua pelo link correto
-          },
-    {
+  },
+
+  {
             título: 'O Herói Perdido',
             descricao: 'Um jovem descobre seu destino e embarca em uma jornada para salvar o mundo.',
             link: 'https://www.amazon.com.br/her%C3%B3i-perdido-S%C3%A9rie-Her%C3%B3is-Olimpo/dp/8580570085' // Substitua pelo link correto
-          },
+  },
 
-    {
+  {
             título: 'Caraval',
             descricao: 'Uma aventura mágica em um mundo de mistério e romance.',
             link: 'https://www.amazon.com.br/Caraval-Stephanie-Garber/dp/8581638562', // Substitua pelo link correto
             
-          },
-    {
+  },
+
+  {
             título: 'Legendário',
             descricao: 'A continuação épica de Caraval, com mais mistérios e reviravoltas.',
             link: 'https://www.amazon.com.br/Lend%C3%A1rio-Stephanie-Garber/dp/8581639062', // Substitua pelo link correto
     
-          },
-    {
+  },
+
+  {
             título: 'Finale',
             descricao: 'O grandioso final da trilogia Caraval, cheio de emoção e revelações.',
             link: 'https://www.amazon.com.br/Finale-Trilogia-Caraval-vol-Gutenberg/dp/8582356714',
-            }   
+  },
+  {
+            título: 'Amanhã, amanhã e ainda outro amanhã',
+            descricao: 'Samson e Sadie são entusiastas da programação nas melhores universidades dos Estados Unidos e um curioso apelo para criarem seu primeiro jogo juntos carrega os leitores na jornada de amadurecimento e sucesso deses dois jovens prodígios',
+            link: 'https://www.amazon.com.br/Amanh%C3%A3-amanh%C3%A3-ainda-outro/dp/655532287X/ref=sr_1_1?crid=SFLGVRT2DENT&dib=eyJ2IjoiMSJ9.VCWVs3ZH3jv67KMO4Hz2tPXEBw08bJQhszOIspAr7mhNYSqpXJDR2hOxfD2V12oDi-Eq0P543Kc3BBQ9835HEx4B14NxJF-Ifg4CScAC5r4KJ-ACa-G06I4OUCiwhbxgEwDWZqElTh76pZNbTcXV0CkxNZxnQkFZEcXgx6Psc1h8Kw3eg3RKC51RUD0ErQY5cHl6_zuQ-Zn3vMLWAf61jr_VoRIVgEbwwDMTjvWJJ-alSEc4o40jpDrPY9eeB8-INwX2hkShw3JmAPpP3ch8WLrfDmi-AtXQ3ji2DSSrfe1w_2JzMXw5o9Ir9dG2mLueo_NoPwaHTLPgkqMdgt3U71eHAr4jVnabeNMAgzZiFQtt72mSdENxKxSU5jjhkJs0.urKcfAkgLzfpYdE_udNnNmHV5zJiFTVLR-XlBTCoG6I&dib_tag=se&keywords=amanha+amanha+e+ainda+outro+amanha&qid=1734384460&sprefix=amanha%2Caps%2C312&sr=8-1&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9',
+  }
         ]
